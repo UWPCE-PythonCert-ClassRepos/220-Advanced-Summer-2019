a7cc65a4d95380fd72761a4ad47faffd5ddc07e3
archwu <archwu0817@gmail.com>
2019-07-05 11:12:06 -0700

    minor modifications for pylint

--- students/ArchWu/lesson01/assignment/inventory_management/electric_appliances_class.py
+++ students/ArchWu/lesson01/assignment/inventory_management/electric_appliances_class.py
@@ -4,18 +4,19 @@ from inventory_class import Inventory
 
 class ElectricAppliances(Inventory):
     """ Electric Appliances class """
-    def __init__(self, productCode, description,
-                 marketPrice,
-                 rentalPrice,
+    def __init__(self, product_code, description,
+                 market_price,
+                 rental_price,
                  brand,
                  voltage):
-        super().__init__(productCode, description, marketPrice,
-                         rentalPrice)
+        super().__init__(product_code, description, market_price,
+                         rental_price)
         # Creates common instance variables from the parent class
         self.brand = brand
         self.voltage = voltage
 
     def return_as_dictionary(self):
+        """return the object as a dict"""
         output_dict = Inventory.return_as_dictionary(self)
         output_dict['brand'] = self.brand
         output_dict['voltage'] = self.voltage
--- students/ArchWu/lesson01/assignment/inventory_management/furniture_class.py
+++ students/ArchWu/lesson01/assignment/inventory_management/furniture_class.py
@@ -5,21 +5,22 @@ from inventory_class import Inventory
 class Furniture(Inventory):
     """ Furniture class, inherits Inventory class """
     def __init__(self,
-                 productCode,
+                 product_code,
                  description,
-                 marketPrice,
-                 rentalPrice,
+                 market_price,
+                 rental_price,
                  material,
                  size):
-        super().__init__(productCode,
+        super().__init__(product_code,
                          description,
-                         marketPrice,
-                         rentalPrice)
+                         market_price,
+                         rental_price)
         # Creates common instance variables from the parent class
         self.material = material
         self.size = size
 
     def return_as_dictionary(self):
+        """return the object as a dict"""
         output_dict = Inventory.return_as_dictionary(self)
         output_dict['material'] = self.material
         output_dict['size'] = self.size
