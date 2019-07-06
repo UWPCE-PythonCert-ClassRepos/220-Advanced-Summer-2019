b75522f94a535808d598c7734df8bcf07d2c8bcf
Andrew Miotke <andrewmiotke@gmail.com>
2019-07-06 12:29:24 -0700

    added readme

---
+++ students/AndrewMiotke/README.md
@@ -0,0 +1,3 @@
+# Andrew Miotke
+
+README for the Py220 Advanced Python Programming class
--- students/AndrewMiotke/lesson01/test_unit2.py
+++ students/AndrewMiotke/lesson01/test_unit2.py
@@ -16,7 +16,7 @@ class SquarerTest(unittest.TestCase):
             self.assertEqual(square, Squarer.calc(num), "Squaring {}".format(num))
 
 
-    def test_negitive_numbers(sel):
+    def test_negitive_numbers(self):
         squares = {
             -1: 1,
             -2: 4,
@@ -30,3 +30,11 @@ class SquarerTest(unittest.TestCase):
 
             if result != square:
                print(f"Squared {num} and got {result} but expected {square}")
+
+
+# Example test
+class Demo(unittest.TestCase):
+    productCode = "AAAA7777"
+    inventory = Inventory(productCode)
+    def compare_dict(self):
+        self.assertDictEqual({}, {}, inventory.returnAsDictionary())
