21e3583bc2d52d336fd2dfde9cc1df07887d9514
Andrew Miotke <andrewmiotke@gmail.com>
2019-07-20 17:14:38 -0700

    Script now works, database is created, and logging is there for fun

--- students/AndrewMiotke/lesson03/assignment/src/basic_operations.py
+++ students/AndrewMiotke/lesson03/assignment/src/basic_operations.py
@@ -4,9 +4,20 @@ import peewee
 import logging
 
 database = peewee.SqliteDatabase('customers.db')
-database.execute_sql('PRAGMA foreign_keys=ON;')
+# database.execute_sql('PRAGMA foreign_keys=ON;')
 database.connect()
 database.execute_sql('drop table if exists customer;')
+
+
+# Logging just for fun
+LOG_FILE = "database_logs.log"
+logger = logging.getLogger()
+logger.setLevel(logging.ERROR)
+LOG_FORMAT = "%(asctime)s\t %(filename)s: %(lineno)-3d %(levelname)s %(message)s)"
+FORMATTER = logging.Formatter(LOG_FORMAT)
+
+FILE_HANDLER = logging.FileHandler(LOG_FILE)
+FILE_HANDLER.setFormatter(FORMATTER)
 
 class BaseModel(peewee.Model):
     class Meta:
@@ -15,7 +26,7 @@ class BaseModel(peewee.Model):
 
 class Customer(BaseModel):
 
-    customer_id = peewee.CharField(primary=True, max_length=30)
+    customer_id = peewee.CharField(primary_key=True, max_length=30)
     name = peewee.CharField(max_length=50)
     lastname = peewee.CharField(max_length=50)
     address = peewee.CharField(max_length=75)
@@ -26,16 +37,6 @@ class Customer(BaseModel):
 
 
 database.create_tables([Customer])
-
-# Logging just for fun
-LOG_FILE = "database_logs.log"
-logger = logging.getLogger()
-logger.setLevel(logging.ERROR)
-LOG_FORMAT = "%(asctime)s\t %(filename)s: %(lineno)-3d %(levelname)s %(message)s)"
-FORMATTER = logging.Formatter(LOG_FORMAT)
-
-FILE_HANDLER = logging.FileHandler(LOG_FILE)
-FILE_HANDLER.setFormat(FORMATTER)
 
 
 # Complete
@@ -55,7 +56,7 @@ def add_customer(customer_id, name, lastname, address, phone_number, email, stat
             customer.save()
             logging.error(f"Successfully saved {customer_id} to database")
 
-    except Exception as e:
+    except:
         logging.error(f"Error creating {customer_id}")
 
 
@@ -80,7 +81,7 @@ def delete_customer(customer_id):
         with database.transaction():
             customer = Customer.get(Customer.customer_id == customer.id)
             customer.delete()
-    except Exception as e:
+    except:
         logging.error(f"Unable to delete user {customer_id}")
         print(e)
 
@@ -101,3 +102,4 @@ def list_active_customers():
     except ValueError:
         logging.error("Unable to count users in database")
         print("Unable to count")
+
