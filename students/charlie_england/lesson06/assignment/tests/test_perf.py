914c700be3fb47908626cd195e56741d10d4c6c5
Charlie England <charlie265@live.com>
2019-08-13 18:06:40 -0700

    commiting finalized lesson06

--- students/charlie_england/lesson06/assignment/tests/test_perf.py
+++ students/charlie_england/lesson06/assignment/tests/test_perf.py
@@ -1,15 +1,16 @@
 """
 check good works the same, and is faster
 """
-
+import sys
+sys.path.insert(0,'../src')
 import poor_perf as p
 import good_perf as g
 
 
 def test_assess_preformance():
     """ compare """
-    poor = p.analyze('tests/lesson06/data/exercise.csv')
-    good = g.analyze('tests/lesson06/data/exercise.csv')
+    poor = p.analyze('..//data//exercise.csv', 'Street')
+    good = g.analyze('..//data//exercise.csv', 'Street')
     poor_elapsed = poor[1] - poor[0]
     good_elapsed = good[1] - good[0]
     assert good_elapsed < poor_elapsed
