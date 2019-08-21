c0bd7ee848b256645faf21725eb396dcf95dd25c
colephalen <colephalen@gmail.com>
2019-08-20 19:05:42 -0700

    oops heres the right one

--- students/colephalen/lesson05/assignment/src/database.py
+++ students/colephalen/lesson05/assignment/src/database.py
@@ -1,35 +1,88 @@
 import csv
-
 from pymongo import MongoClient
 
-mongo = MongoClient('localhost:27017')
-db = mongo["assignment"]
+import threading
+
+mongo = MongoClient("mongodb://localhost:27017")
+
+db = mongo["norton"]
 
 
 def import_data(data_dir, *files):
     for filepath in files:
+        print('opening file: {}'.format(filepath))
         collection_name = filepath.split(".")[0]
+        print("creating collection: {}".format(collection_name))
 
-        print("opening", "/".join([data_dir, filepath]))
         with open("/".join([data_dir, filepath])) as file:
-            reader = csv.reader(file, delimiter=',')
+            reader = csv.reader(file, delimiter=",")
 
             header = False
+
+            table = db[collection_name]
+
             for row in reader:
                 if not header:
-                    header = [h.strip('\ufeff') for h in row]
+                    header = [h for h in row]
                 else:
-                    data = {header[i]: v for i, v in enumerate(row)}
-                    print(data)
-                    cursor = db[collection_name]
-                    cursor.insert_one(data)
+                    data = {
+                        header[i]: v for i, v in enumerate(row)
+                    }
+
+                    try:
+                        table.insert_one(data)
+                    except Exception as e:
+                        print(e)
+
+                    # pprint(data)
 
 
-import_data(
-    "/Users/colephalen/220-Advanced-Summer-2019/students/colephalen/lesson05/assignment/data",
-    "product.csv",
-    "customers.csv",
-    "rental.csv"
-)
+def import_data_multithread(filepath):
+    print('opening file: {}'.format(filepath))
+    collection_name = filepath.split(".")[0]
+    print("creating collection: {}".format(collection_name))
 
+    with open(filepath) as file:
+        reader = csv.reader(file, delimiter=",")
+
+        header = False
+
+        table = db[collection_name]
+
+        for row in reader:
+            if not header:
+                header = [h for h in row]
+            else:
+                data = {header[i]: v for i, v in enumerate(row)}
+
+                try:
+                    table.insert_one(data)
+                except Exception as e:
+                    print(e)
+
+
+if __name__ == '__main__':
+    db['customer'].drop()
+    db['rental'].drop()
+    db['product'].drop()
+    # import_data('rentals/data', 'customer.csv', 'product.csv', 'rental.csv')
+
+    files = [
+        "rentals/data/product.csv",
+        "rentals/data/rental.csv",
+        "rentals/data/customer.csv"
+    ]
+
+    threads = []
+
+    for filepath in files:
+        thread = threading.Thread(target=import_data_multithread,
+                                  args=(filepath,)
+                                  )
+        thread.start()
+        threads.append(thread)
+
+        # # might not apply:
+        for thread in threads:
+            thread.join()
 
