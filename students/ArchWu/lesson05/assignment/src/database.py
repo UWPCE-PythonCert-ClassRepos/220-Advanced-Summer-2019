702274ff67a3d6457c1aa7721dad1b82b824dd2d
archwu <archwu0817@gmail.com>
2019-08-28 19:52:00 -0700

    import done

--- students/ArchWu/lesson05/assignment/src/database.py
+++ students/ArchWu/lesson05/assignment/src/database.py
@@ -7,23 +7,37 @@ As a HP Norton salesperson I want to see a list of the names and contact details
 """
 
 from pymongo import MongoClient
+from pprint import pprint
+
 import csv
 
-client = MongoClient(port=27017)
-db = client.admin
+client = MongoClient('mongodb://localhost:27017')
+db = client["norton"]
+# db = client.admin
 
 
-def import_data():
-    with open('customers.csv', newline="") as file:
-        csv_reader = csv.reader(file, delimiter=',')
-        for row in csv_reader:
-            if line_count == 0:
-                print(f'Column names are {", ".join(row)}')
-                line_count += 1
-            else:
-                line_count += 1
-                
-        print(f'Processed {line_count} lines.')
+def import_data(data_dir, files):
+    for filepath in files:
+        print("Opening file %s" %filepath)
+        collection_name = filepath.split('.')[0]
+        print("Creating collection %s" %collection_name)
+        with open('/'.join([data_dir, filepath]), encoding='utf-8-sig') as file:
+            csv_reader = csv.reader(file, delimiter=',')
+            header = False
+            for row in csv_reader:
+                if not header:
+                    header = [h for h in row]
+                else:
+                    data = {
+                        header[i]:v
+                        for i,v in enumerate(row)
+                    }
+                    table = db['norton']
+                    try:
+                        table.insert_one(data)
+                    except Exception as e:
+                        print(e)
+                    pprint(data)
 
 def show_available_products():
     pass
@@ -33,3 +47,16 @@ def list_unique_products():
 
 def show_rentals():
     pass
+
+if __name__ == "__main__":
+    db["customer"].drop()
+    db["product"].drop()
+    db["rental"].drop()
+
+    files = [
+        "customers.csv",
+        "product.csv",
+        "rental.csv",
+    ]
+    import_data('data', files)
+    print('Done!')
