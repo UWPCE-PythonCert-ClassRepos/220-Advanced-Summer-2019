a03bddfac41872fc74cb65f245dbf2f4b4339195
Fred Ballyns <FredBallyns@gmail.com>
2019-09-01 15:21:39 -0700

    lesson 6 done

---
+++ students/FredBallyns/lesson06/assignment/tests/test_perf.py
@@ -0,0 +1,17 @@
+"""
+check good works the same, and is faster
+"""
+
+import poor_perf as p
+import good_perf as g
+
+
+def test_assess_preformance():
+    """ compare """
+    poor = p.analyze('../data/exercise.csv')
+    good = g.analyze('../data/exercise.csv')
+    poor_elapsed = poor[1] - poor[0]
+    good_elapsed = good[1] - good[0]
+    assert good_elapsed < poor_elapsed
+    assert poor[2] == good[2]
+    assert poor[3] == good[3]
