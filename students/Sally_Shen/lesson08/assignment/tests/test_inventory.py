8a14ea66ed153f5458dbeeba5d3ad48a52813247
sallysy8 <sallysy8@hotmail.com>
2019-09-03 18:07:42 -0700

    Add assign08

--- students/Sally_Shen/lesson08/assignment/tests/test_inventory.py
+++ students/Sally_Shen/lesson08/assignment/tests/test_inventory.py
@@ -2,18 +2,13 @@
     Autograde Lesson 8 assignment
 
 """
+import sys
+sys.path.append("../src/")
 
-import pytest
+import inventory
 
-import inventory as l
-
-
-
-def test_add_furniture(invoice_file, customer_name, item_code, item_description, item_monthly_price):
-
-
-
-def single_customer(customer_name, invoice_file):
-
-
-    
+inventory.add_furniture("rented_items.csv", "Elisa Miles", "LR04", "Leather Sofa", 25)
+inventory.add_furniture("rented_items.csv", "Edward Data", "KT78", "Kitchen Table", 10)
+inventory.add_furniture("rented_items.csv", "Alex Gonzales", "BR02", "Queen Mattress", 17)
+create_invoice = inventory.single_customer("Susan Wong", "rented_items.csv")
+create_invoice("..\\data\\test_items.csv")
