df9cbb26d458cfe8d86d9b9efecc07590ed428fe
archwu <archwu0817@gmail.com>
2019-09-09 16:01:35 -0700

    done

--- students/ArchWu/lesson06/assignment/tests/test_perf.py
+++ students/ArchWu/lesson06/assignment/tests/test_perf.py
@@ -8,8 +8,8 @@ import good_perf as g
 
 def test_assess_preformance():
     """ compare """
-    poor = p.analyze('tests/lesson06/data/exercise.csv')
-    good = g.analyze('tests/lesson06/data/exercise.csv')
+    poor = p.analyze('data/exercise.csv','Unit')
+    good = g.analyze('data/exercise.csv','Unit')
     poor_elapsed = poor[1] - poor[0]
     good_elapsed = good[1] - good[0]
     assert good_elapsed < poor_elapsed
