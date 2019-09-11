e75e76530ada93d9ec28a7d206f441df22401c1d
George Kim <gjkim44@gmail.com>
2019-09-10 18:42:41 -0700

    adding lesson 4

--- students/GKim/lesson04/assignment/tests/test_gradel04.py
+++ students/GKim/lesson04/assignment/tests/test_gradel04.py
@@ -6,69 +6,70 @@
 
 """
 
-import pytest
+import sys
+sys.path.append("../src/")
 
+import pytest
 import basic_operations as l
 
-import logging
-
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-
-fh = logging.FileHandler("db.log")
-logger.addHandler()
-
-logger.addHandler(fh)
-
-logging.basicConfig(level=logging.DEBUG, filename="bd.log")
 
 
 @pytest.fixture
 def _add_customers():
-    return  iter([
-        ("123", "Name", "Lastname", "Address", "phone", "email", "active", 999),
-        ("456", "Name", "Lastname", "Address", "phone", "email", "inactive", 10),
-        ("123", "Name", "Lastname", "Address", "phone", "email", "active", 999),
-        ("789", "Name", "Lastname", "Address", "phone", "email", "active", 0),
-        ("345", "Name", "Lastname", "Address", "phone", "email", "active", -10),
-        ("0123", "Name", "Lastname", "Address", "phone", "email", "active", 999),
-        ("777", "Name", "Lastname", "Address", "phone", "email", "active", 999)
-    ])
-
-    # first_row = next(it)
+    return [
+        ("123", "a", "Lastname", "Address", "phone", "email", "active", 999),
+        ("456", "b", "Lastname", "Address", "phone", "email", "inactive", 10),
+        ("333", "c", "Lastname", "Address", "phone", "email", "active", 999),
+        ("789", "d", "Lastname", "Address", "phone", "email", "active", 0),
+        ("345", "e", "Lastname", "Address", "phone", "email", "active", -10),
+        ("0123", "f", "Lastname", "Address", "phone", "email", "active", 999),
+        ("777", "g", "Lastname", "Address", "phone", "email", "active", 999)
+    ]
 
 @pytest.fixture
 def _search_customers(): # needs to del with database
     return [
-        [("998", "Name", "Lastname", "Address", "phone", "email", "active", 999),
-         ("997", "Name", "Lastname", "Address", "phone", "email", "inactive", 10)],
-        ("998", "000")
+        [("998", "a", "Lastname", "Address", "phone", "email", "active", 999),
+         ("997", "b", "Lastname", "Address", "phone", "email", "inactive", 10)],
+        ("997", "000")
     ]
 @pytest.fixture
 def _delete_customers(): # needs to del with database
-    return (row for row in [
-        ("898", "Name", "Lastname", "Address", "phone", "email", "active", 999),
-        ("897", "Name", "Lastname", "Address", "phone", "email", "inactive", 10)
+    return(row for row in [
+        ("898", "a", "Lastname", "Address", "phone", "email", "active", 999),
+        ("897", "b", "Lastname", "Address", "phone", "email", "inactive", 10)
     ])
 
 @pytest.fixture
 def _update_customer_credit(): # needs to del with database
     return [
-        ("798", "Name", "Lastname", "Address", "phone", "email", "active", 999),
-        ("797", "Name", "Lastname", "Address", "phone", "email", "inactive", 10),
-        ("796", "Name", "Lastname", "Address", "phone", "email", "inactive", -99)
+        ("798", "a", "Lastname", "Address", "phone", "email", "active", 999),
+        ("797", "b", "Lastname", "Address", "phone", "email", "inactive", 10),
+        ("796", "c", "Lastname", "Address", "phone", "email", "inactive", -99)
     ]
 
 @pytest.fixture
 def _list_active_customers():
     return [
-        ("598", "Name", "Lastname", "Address", "phone", "email", "active", 999),
-        ("597", "Name", "Lastname", "Address", "phone", "email", "inactive", 10),
-        ("596", "Name", "Lastname", "Address", "phone", "email", "inactive", 99),
-        ("595", "Name", "Lastname", "Address", "phone", "email", "active", 999),
-        ("594", "Name", "Lastname", "Address", "phone", "email", "active", 10),
-        ("593", "Name", "Lastname", "Address", "phone", "email", "active", 99)
+        ("598", "a", "Lastname", "Address", "phone", "email", "active", 999),
+        ("597", "b", "Lastname", "Address", "phone", "email", "inactive", 10),
+        ("596", "c", "Lastname", "Address", "phone", "email", "inactive", 99),
+        ("595", "d", "Lastname", "Address", "phone", "email", "active", 999),
+        ("594", "e", "Lastname", "Address", "phone", "email", "active", 10),
+        ("593", "f", "Lastname", "Address", "phone", "email", "active", 99)
     ]
+
+def setup_function(self):
+    """ setup any state tied to the execution of the given method in a
+    class.  setup_method is invoked for every test method of a class.
+    """
+    l.create_tables()
+
+def teardown_function(self):
+    """ teardown any state that was previously setup with a setup_method
+    call.
+    """
+    l.drop_tables()
 
 def test_list_active_customers(_list_active_customers):
     """ actives """
@@ -84,11 +85,10 @@ def test_list_active_customers(_list_active_customers):
                        )
     actives = l.list_active_customers()
 
-    assert actives == 2
+    assert actives == 4
 
     for customer in _list_active_customers:
         l.delete_customer(customer[0])
-
 
 
 def test_add_customer(_add_customers):
@@ -105,13 +105,12 @@ def test_add_customer(_add_customers):
                        )
         added = l.search_customer(customer[0])
         assert added["name"] == customer[1]
-        assert added["lastname"] == customer[2]
+        assert added["last_name"] == customer[2]
         assert added["email"] == customer[5]
         assert added["phone_number"] == customer[4]
 
     for customer in _add_customers:
         l.delete_customer(customer[0])
-
 
 
 def test_search_customer(_search_customers):
@@ -132,11 +131,11 @@ def test_search_customer(_search_customers):
 
     result = l.search_customer(_search_customers[1][0])
     assert result["name"] == _search_customers[0][1][1]
-    assert result["lastname"] == _search_customers[0][1][2]
+    assert result["last_name"] == _search_customers[0][1][2]
     assert result["email"] == _search_customers[0][1][5]
     assert result["phone_number"] == _search_customers[0][1][4]
 
-    for customer in _search_customers:
+    for customer in _search_customers[0]:
         l.delete_customer(customer[0])
 
 
@@ -159,6 +158,7 @@ def test_delete_customer(_delete_customers):
         deleted = l.search_customer(customer[0])
         assert deleted == {}
 
+
 def test_update_customer_credit(_update_customer_credit):
     """ update """
     for customer in _update_customer_credit:
@@ -178,4 +178,4 @@ def test_update_customer_credit(_update_customer_credit):
     l.update_customer_credit("796", 500)
     with pytest.raises(ValueError) as excinfo:
         l.update_customer_credit("00100", 1000) # error
-        assert 'NoCustomer'  in str(excinfo.value)
+        assert 'NoCustomer' in str(excinfo.value)
