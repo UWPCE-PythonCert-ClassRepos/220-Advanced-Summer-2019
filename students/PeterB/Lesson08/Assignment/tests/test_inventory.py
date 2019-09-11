c5832883884c5b01678fa82b412696432acdaed0
pberhe <pberhe90@gmail.com>
2019-09-11 16:49:23 -0700

    'lesson08'

---
+++ students/PeterB/Lesson08/Assignment/tests/test_inventory.py
@@ -0,0 +1,9 @@
+import inventory
+import sys
+sys.path.append("../Assignment/")
+
+inventory.add_furniture("rented_items.csv", "Elisa Miles", "LR04", "Leather Sofa", 25)
+inventory.add_furniture("rented_items.csv", "Edward Data", "KT78", "Kitchen Table", 10)
+inventory.add_furniture("rented_items.csv", "Alex Gonzales", "BR02", "Queen Mattress", 17)
+create_invoice = inventory.single_customer("Susan Wong", "rented_items.csv")
+create_invoice("..\\data\\test_items.csv")
