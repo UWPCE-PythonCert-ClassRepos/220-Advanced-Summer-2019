5e9580e8b69e762317766a6367e253c65b424fd1
Rimlee Talukdar <rimlee.talukdar@gmail.com>
2019-09-15 16:22:18 -0700

    Assingment 5 solution

--- students/rimlee_talukdar/lesson05/assignment/tests/test_database.py
+++ students/rimlee_talukdar/lesson05/assignment/tests/test_database.py
@@ -7,33 +7,41 @@ import pytest
 
 import database as l
 
+
 @pytest.fixture
 def _show_available_products():
     return {
-        'P000001': {'description': 'Chair Red leather', 'product_type': 'livingroom',
-                    'quantity_available': '21'},
-        'P000002': {'description': 'Table Oak', 'product_type': 'livingroom',
-                    'quantity_available': '4'},
-        'P000003': {'description': 'Couch Green cloth', 'product_type': 'livingroom',
-                    'quantity_available': '10'},
-        'P000004': {'description': 'Dining table Plastic', 'product_type': 'Kitchen',
-                    'quantity_available': '23'},
-        'P000005': {'description': 'Stool Black ash', 'product_type': 'Kitchen',
-                    'quantity_available': '12'}
-        }
+        'prd001': {'description': '60-inch TV stand', 'product_type': 'livingroom', 'quantity_available': '3'},
+        'prd003': {'description': 'Acacia kitchen table', 'product_type': 'kitchen', 'quantity_available': '7'},
+        'prd004': {'description': 'Queen bed', 'product_type': 'bedroom', 'quantity_available': '10'},
+        'prd005': {'description': 'Reading lamp', 'product_type': 'bedroom', 'quantity_available': '20'},
+        'prd006': {'description': 'Portable heater', 'product_type': 'bathroom', 'quantity_available': '14'},
+        'prd008': {'description': 'Smart microwave', 'product_type': 'kitchen', 'quantity_available': '30'},
+        'prd010': {'description': '60-inch TV', 'product_type': 'livingroom', 'quantity_available': '3'}
+    }
 
 @pytest.fixture
 def _show_rentals():
     return {
-        'C000001': {'name': 'Shea Boehm', 'address': '3343 Sallie Gateway',
-                    'phone_number': '508.104.0644', 'email': 'Alexander.Weber@monroe.com'},
-        'C000003': {'name': 'Elfrieda Skiles', 'address': '3180 Mose Row',
-                    'phone_number': '839)825-0058', 'email': 'Mylene_Smitham@hannah.co.uk'}
+        'user001': {
+            'name': 'Elisa Miles',
+            'address': '4490 Union Street',
+            'phone_number': '206-922-0882',
+            'email': 'elisa.miles@yahoo.com'
+        },
+        'user003': {
+            'name': 'Andy Norris',
+            'address': '348 Terra Street',
+            'phone_number': '206-309-2533',
+            'email': 'andy.norris@gmail.com'
         }
+    }
+
+
 
 def test_import_data():
     """ import """
-    data_dir = os.path.dirname(os.path.abspath(__file__))
+    data_dir = os.path.dirname(os.getcwd() + '/data/')
     added, errors = l.import_data(data_dir, "products.csv", "customers.csv", "rentals.csv")
 
     for add in added:
@@ -42,15 +50,18 @@ def test_import_data():
     for error in errors:
         assert isinstance(error, int)
 
-    assert added == (5, 11, 9)
+    assert added == (10, 10, 9)
     assert errors == (0, 0, 0)
+
 
 def test_show_available_products(_show_available_products):
     """ available products """
-    students_response = l.show_available_products()
-    assert students_response == _show_available_products
+    available_products_response = l.show_available_products()
+    print(available_products_response)
+    assert available_products_response == _show_available_products
 
 def test_show_rentals(_show_rentals):
     """ rentals """
-    students_response = l.show_rentals("P000003")
-    assert students_response == _show_rentals
+    rentals_response = l.show_rentals("prd005")
+    print(rentals_response)
+    assert rentals_response == _show_rentals
